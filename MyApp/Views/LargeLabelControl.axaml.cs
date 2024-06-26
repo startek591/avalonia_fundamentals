fc8463690e6ded29a2782253261a16d472dc135b
jonathan_fambro <startek591@gmail.com>
2024-06-25 21:57:31 -0400

    Templated Controls

---
+++ MyApp/Views/LargeLabelControl.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace MyApp;
+
+public class LargeLabelControl : TemplatedControl
+{
+    public static readonly StyledProperty<string> LargeTextProperty = AvaloniaProperty.Register<LargeLabelControl, string>("LargeText", "LARGE TEXT");
+
+    public string LargeText
+    {
+        get => GetValue(LargeTextProperty);
+        set => SetValue(LargeTextProperty, value);
+    }
+
+    public static readonly StyledProperty<string> SmallTextProperty = AvaloniaProperty.Register<LargeLabelControl, string>("SmallText", "SMALL TEXT");
+
+    public string SmallText
+    {
+        get => GetValue(SmallTextProperty);
+        set => SetValue(SmallTextProperty, value);
+    }
+
+}
