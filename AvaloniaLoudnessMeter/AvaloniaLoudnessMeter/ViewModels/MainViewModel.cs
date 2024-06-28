e377a0191a7b2c1f26300dd8fff24bc6e17c555c
jonathan_fambro <startek591@gmail.com>
2024-06-28 15:06:52 -0400

    Animate Popup

--- AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/ViewModels/MainViewModel.cs
+++ AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/ViewModels/MainViewModel.cs
@@ -1,10 +1,21 @@
-﻿using ReactiveUI;
+﻿using System.Windows.Input;
+using Avalonia.Input;
+using ReactiveUI;
 namespace AvaloniaLoudnessMeter.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
     private string? _boldTitle = "AVALONIA";
     private string? _regularTitle = "LOUDNESS METER";
+
+    private bool? _channelConfigurationListIsOpen = false;
+
+    public MainViewModel()
+    {
+        ChannelConfigurationButtonPressedCommand = ReactiveCommand.Create(ChannelConfigurationButtonPressed);
+    }
+
+    public ICommand ChannelConfigurationButtonPressedCommand { get; }
 
     public string? BoldTitle
     {
@@ -18,4 +29,14 @@ public partial class MainViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _regularTitle, value);
     }
 
+    public bool? ChannelConfigurationListIsOpen
+    {
+        get => _channelConfigurationListIsOpen;
+        set => this.RaiseAndSetIfChanged(ref _channelConfigurationListIsOpen, value);
+    }
+
+    private void ChannelConfigurationButtonPressed()
+    {
+        ChannelConfigurationListIsOpen = !ChannelConfigurationListIsOpen;
+    }
 }
--- AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Views/MainView.axaml.cs
+++ AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Views/MainView.axaml.cs
@@ -1,11 +1,49 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Media;
+using AvaloniaLoudnessMeter.ViewModels;
+using System;
 
 namespace AvaloniaLoudnessMeter.Views;
 
 public partial class MainView : UserControl
 {
+    private Control mChannelConfigPopup;
+    private Control mChannelConfigButton;
+
+    private Control mMainGrid;
     public MainView()
     {
         InitializeComponent();
+        mChannelConfigButton = this.FindControl<Control>("ChannelConfigurationButton") ?? throw new Exception("Cannot find Channel Configuration Button by name");
+        mChannelConfigPopup = this.FindControl<Control>("ChannelConfigurationPopup") ?? throw new Exception("Cannot find Channel Configuration Popup by name");
+        mMainGrid = this.FindControl<Control>("MainGrid") ?? throw new Exception("Cannot find Main Grid by name");
+
+        mChannelConfigButton.PointerPressed += InputElement_OnPointerPressed;
+        // Listen to changes in popup visibility to adjust its position
+        mChannelConfigPopup.PropertyChanged += (sender, args) =>
+        {
+            if (args.Property.Name == nameof(IsVisible) && mChannelConfigPopup.IsVisible)
+            {
+                this.UpdatePopupPosition();
+            }
+        };
+
     }
+
+    private void UpdatePopupPosition()
+    {
+        var position = mChannelConfigButton.TranslatePoint(new Point(), mMainGrid)
+            ?? throw new Exception("Cannot get TranslatePoint from Configuration Button");
+
+        mChannelConfigPopup.Margin = new Thickness(
+            position.X,
+            0,
+            0,
+            mMainGrid.Bounds.Height - position.Y - mChannelConfigButton.Bounds.Height);
+    }
+
+
+    private void InputElement_OnPointerPressed(object sender, PointerPressedEventArgs e) => ((MainViewModel)DataContext).ChannelConfigurationButtonPressedCommand.Execute(null);
 }
