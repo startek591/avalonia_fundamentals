072dc17d7302576a29a5ed7e5f5d81f2a87330d5
jonathan_fambro <startek591@gmail.com>
2024-09-23 18:50:46 -0400

    Get Started

--- GetStartedApp/Views/MainWindow.axaml.cs
+++ GetStartedApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace GetStartedApp.Views;
 
@@ -7,5 +8,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public void ButtonClicked(object? source, RoutedEventArgs args)
+    {
+        if (double.TryParse(celsius.Text, out double C))
+        {
+            var F = C * (9d / 5d) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
     }
 }
