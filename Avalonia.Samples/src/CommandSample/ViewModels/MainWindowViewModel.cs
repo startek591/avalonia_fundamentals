7c0c728301e527f87df57175985660937dfd95e7
jonathan_fambro <startek591@gmail.com>
2024-09-24 23:30:32 -0400

    ICommand, ReactiveCommand, and long running Tasks

--- Avalonia.Samples/src/CommandSample/ViewModels/MainWindowViewModel.cs
+++ Avalonia.Samples/src/CommandSample/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ReactiveUI;
 using System.Collections.ObjectModel;
 using System;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace CommandSample.ViewModels;
@@ -10,6 +11,15 @@ public class MainWindowViewModel : ViewModelBase
     public MainWindowViewModel()
     {
         OpenThePodBayDoorsDirectCommand = ReactiveCommand.Create(OpenThePodBayDoors);
+
+        IObservable<bool> canExecuteFellowRobotCommand =
+            this.WhenAnyValue(vm => vm.RobotName, (name) => !string.IsNullOrEmpty(name));
+
+        OpenThePodBayDoorsFellowRobotCommand =
+            ReactiveCommand.Create<string?>(name => OpenThePodBayDoorsFellowRobot(name), canExecuteFellowRobotCommand);
+
+        OpenThePodBayDoorsAsyncCommand = ReactiveCommand.CreateFromTask(OpenThePodBayDoorsAsync);
+
     }
 
     public ICommand OpenThePodBayDoorsDirectCommand { get; }
@@ -18,8 +28,42 @@ public class MainWindowViewModel : ViewModelBase
     {
         ConversationLog.Clear();
         AddToConvo("I'm sorry, Dave, I'm afraid I can't do that.");
-
     }
+
+    public ICommand OpenThePodBayDoorsFellowRobotCommand { get; }
+
+    private void OpenThePodBayDoorsFellowRobot(string? robotName)
+    {
+        ConversationLog.Clear();
+        AddToConvo($"Hello {robotName}, the Pod Bay is open :-)");
+    }
+
+    private string? _RobotName;
+
+    public string? RobotName
+    {
+        get => _RobotName;
+        set => this.RaiseAndSetIfChanged(ref _RobotName, value);
+    }
+
+    public ICommand OpenThePodBayDoorsAsyncCommand { get; }
+
+    private async Task OpenThePodBayDoorsAsync()
+    {
+        ConversationLog.Clear();
+        AddToConvo("Preparing to open the Pod Bay...");
+
+        await Task.Delay(1000);
+
+        AddToConvo("Depressurizing Airlock....");
+        await Task.Delay(2000);
+
+        AddToConvo("Retracting blast doors");
+        await Task.Delay(2000);
+
+        AddToConvo("Pod Bay is open to space!");
+    }
+
 
     public ObservableCollection<string> ConversationLog { get; } = new ObservableCollection<string>();
 
