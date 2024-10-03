6f53f6340e4673810b90f504dc06981a937bacb4
jonathan_fambro <startek591@gmail.com>
2024-10-03 19:36:36 -0400

    View Model Basics

--- Avalonia-Loudness-Meter/ViewModels/MainWindowViewModel.cs
+++ Avalonia-Loudness-Meter/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace Avalonia_Loudness_Meter.ViewModels;
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace Avalonia_Loudness_Meter.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-
+    [Reactive]
+    public string BoldTitle { get; set; } = "AVALONIA";
+    [Reactive] public string RegularTitle { get; set; } = "LOUDNESS METER";
 }
